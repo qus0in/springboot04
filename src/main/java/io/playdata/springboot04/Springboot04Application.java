2010a1a101a271f88cb00ac51786796bdf2ae29b
qus0in <qus0in@gmail.com>
2023-05-04 12:38:31 +0900

    Initial commit

---
+++ src/main/java/io/playdata/springboot04/Springboot04Application.java
@@ -0,0 +1,13 @@
+package io.playdata.springboot04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot04Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/playdata/springboot04/Springboot04ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.playdata.springboot04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
